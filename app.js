9ca4869bd78b28ec5ff1baa7b24ebfc4a74ae8fc
Yago <yago@lumanet.com.br>
2022-05-03 17:27:05 -0300

    package

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.get('/', (req, res) =>{
-    res.send('Teste')
+    res.send('Testee')
 })
 
 app.listen(3000)
